02fa2048d004b07e3f2b2caa20a8a9843f980e93
Baptiste Fontaine <batifon@yahoo.fr>
2013-06-03 15:10:28 +0200

    initial commit.

---
+++ README.md
@@ -0,0 +1,18 @@
+# ud
+
+**ud** is a web scrapping tool for [Urban Dictionnary][urban-dic]. The website
+doesn’t have an API, so this little tool allows you to find the definition(s) of
+a word by scrapping its pages.
+
+## Install
+
+TODO
+
+## Usage
+
+```sh
+ud <word>
+```
+
+
+[urban-dic]: http://www.urbandictionary.com/define.php?term=totolala
---
+++ src/ud.rb
@@ -0,0 +1,78 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+
+require 'uri'
+require 'open-uri'
+require 'nokogiri'
+
+module UD
+
+    # Get the search URL to query for a given term
+    def UD.search_url(term)
+        param = URI.encode_www_form('term' => term)
+        "http://www.urbandictionary.com/define.php?#{param}"
+    end
+
+    # Get the thumbs (up/down) for a list of definitions (ids)
+    def UD.thumbs(ids)
+
+        param = URI.encode_www_form('ids' => ids.join(','))
+        json = open "http://api.urbandictionary.com/v0/uncacheable?#{param}"
+
+        response = JSON.parse(json.read)
+        thumbs   = {}
+
+        response['thumbs'].each do |t|
+
+            thumbs[t['defid']] = {
+                :up   => t['thumbs_up'],
+                :down => t['thumbs_down']
+            }
+
+        end
+
+        thumbs
+    end
+
+    # Get the text of an element
+    def UD.text(el)
+        el.text.strip.gsub(/\r/, "\n")
+    rescue
+        ''
+    end
+
+    # Query the website and return a list of definitions for the provided term.
+    # This list may be empty if there's no results. It only scraps the first
+    # page of results, since it's generally sufficient.
+    def UD.query(term)
+        url = search_url(term)
+        doc = Nokogiri::HTML(open(url))
+
+        return [] unless doc.css('#not_defined_yet').empty?
+
+        words = doc.css('.word[data-defid]')
+        ids   = words.map { |w| w.attr('data-defid') }
+
+        thumbs = thumbs(ids)
+
+        ids.map do |id|
+
+            word = text doc.css(".word[data-defid=\"#{id}\"] > span").first
+            body = doc.css("#entry_#{id}")
+            t    = thumbs[id.to_i] || {}
+
+            {
+                :id => id,
+                :word => word,
+                :definition => text(body.css('.definition')),
+                :example => text(body.css('.example')),
+                :upvotes => t[:up],
+                :downvotes => t[:down]
+
+            }
+
+        end
+
+    end
+
+end
