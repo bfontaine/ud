a8f1299bcdbf57095394aaf572bc9ea2f5c148d7
Baptiste Fontaine <batifon@yahoo.fr>
2014-02-02 20:48:01 +0100

    FakeWeb was blocking Coveralls request

--- tests/fake_responses.rb
+++ tests/fake_responses.rb
@@ -6,7 +6,7 @@ require 'fakeweb'
 RESPONSES_DIR = "#{File.expand_path(File.dirname(__FILE__))}/responses"
 BASE_URL = 'http://api.urbandictionary.com/v0/define'
 
-FakeWeb.allow_net_connect = false
+FakeWeb.allow_net_connect = %r[^https?://coveralls\.io]
 
 Dir["#{RESPONSES_DIR}/*.json"].each do |f|
   next if f !~ /\/(\w+)\.json$/
