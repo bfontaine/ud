8d114b47d4f6178bcca0d062140e73933348fe8c
Baptiste Fontaine <b@ptistefontaine.fr>
2016-11-27 19:11:34 +0100

    Respect the style guide

--- lib/ud.rb
+++ lib/ud.rb
@@ -1,19 +1,18 @@
 # -*- coding: UTF-8 -*-
 
-require 'uri'
-require 'json'
-require 'open-uri'
+require "uri"
+require "json"
+require "open-uri"
 
-require File.dirname(__FILE__) + '/ud/formatting'
+require File.dirname(__FILE__) + "/ud/formatting"
 
 # This module provide some methods to scrape definitions from the Urban
 # Dictionary website.
 module UD
   class << self
-
     # @return [String] the current gem's version
     def version
-      '0.2.5'
+      "0.2.5"
     end
 
     # Get the search URL to query for a given term.
@@ -21,8 +20,8 @@ module UD
     #                      are allowed.
     # @param api [Boolean] truthy if the API URL should be used.
     # @return [String]
-    def search_url(term='', api=true)
-      param = URI.encode_www_form('term' => term)
+    def search_url(term, api = true)
+      param = URI.encode_www_form("term" => term)
 
       if api
         "http://api.urbandictionary.com/v0/define?#{param}"
@@ -31,7 +30,7 @@ module UD
       end
     end
 
-    def random_url(api=true)
+    def random_url(api = true)
       if api
         "http://api.urbandictionary.com/v0/random"
       else
@@ -43,7 +42,7 @@ module UD
     # @param term [String] the term to search for. It must be a string, spaces
     #                      are allowed.
     # @return [Nil]
-    def open_url(term='')
+    def open_url(term = "")
       system open_cmd, search_url(term, false)
     end
 
@@ -56,14 +55,23 @@ module UD
     #                    options may be added in the future.
     # @return [Array<Hash>]
     def query(term, *opts)
+      parse_response(open(search_url(term)).read, *opts)
+    end
 
-      opts = {:count => 1, :ratio => 0.0}.merge(opts[0] || {})
+
+    # @param opts [Hash] options. This is used by the command-line tool.
+    #                    +:count+ is the maximum number of results to return,
+    #                    +:ratio+ is the minimum upvotes/downvotes ratio. Other
+    #                    options may be added in the future.
+    # @return [Array<Hash>]
+    def parse_response(text, *opts)
+      opts = { :count => 1 }.merge(opts[0] || {})
 
       return [] if opts[:count] <= 0
 
-      resp = JSON.parse(open(search_url term).read, :symbolize_names => true)
+      resp = JSON.parse(text, :symbolize_names => true)
 
-      resp[:list].map do |res|
+      results = resp[:list].map do |res|
         {
           :id         => res[:defid],
           :word       => res[:word],
@@ -72,11 +80,17 @@ module UD
           :definition => res[:definition].strip,
           :example    => res[:example].strip,
           :upvotes    => res[:thumbs_up],
-          :downvotes  => res[:thumbs_down]
+          :downvotes  => res[:thumbs_down],
         }
-      end.keep_if do |d|
-        d[:upvotes]/[d[:downvotes], 0.1].max.to_f >= opts[:ratio]
-      end.take opts[:count]
+      end
+
+      results.select! do |d|
+        next true if d[:downvotes] == 1
+
+        d[:upvotes] / d[:downvotes].to_f >= opts[:ratio]
+      end if opts[:ratio]
+
+      results.take opts[:count]
     end
 
     # Format results for output
@@ -84,7 +98,7 @@ module UD
     #                        +UD.query+.
     # @param color [Boolean] colored output
     # @return [String]
-    def format_results(results, color=true)
+    def format_results(results, color = true)
       UD::Formatting.text(results, color)
     end
 
